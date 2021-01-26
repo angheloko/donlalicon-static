cddd0fe988880a79a2249885d8c8684995635ec4
Don <don@gobear.com>
2021-01-26 17:38:32 +0800

    - More styling.

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,19 +1,21 @@
 <template>
-  <article>
+  <article class="blog">
     <h1>
       {{ blog.title }}
     </h1>
     <h2>
       {{ blog.lead }}
     </h2>
-    <div>
+    <div class="blog__date">
       {{ blog.createdAt | toDate }}
     </div>
     <figure>
       <img :src="blog.cover.image" :alt="blog.cover.alt">
-      <figcaption>{{ blog.cover.caption }}</figcaption>
+      <figcaption>
+        {{ blog.cover.caption }}
+      </figcaption>
     </figure>
-    <nuxt-content :document="blog" />
+    <nuxt-content :document="blog" class="blog__content" />
   </article>
 </template>
 <script>
