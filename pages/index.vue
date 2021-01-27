e3bcc57eed4b62e0356a6b2a6031626c34a3bf63
Don <don@gobear.com>
2021-01-27 08:50:53 +0800

    - Styled home page list of blogs.

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,40 @@
 <template>
-  <div class="container">
-    Testing
+  <div>
+    <NuxtLink
+      v-for="blog of blogs"
+      :key="blog.slug"
+      :to="{ name: 'blog-slug', params: { slug: blog.slug } }"
+      class="flex mb-4 bg-gray-100 p-4 rounded no-underline text-gray-800 transition-colors duration-300 ease-linear hover:text-gray-800 hover:bg-gray-200"
+    >
+      <div class="flex-grow flex flex-col">
+        <h2 class="font-serif">
+          {{ blog.title }}
+        </h2>
+        <h3 class="flex-grow">
+          {{ blog.subtitle }}
+        </h3>
+        <div class="text-sm text-gray-600">
+          {{ blog.createdAt | toDate }}
+        </div>
+      </div>
+      <img :src="blog.cover.thumb" :alt="blog.cover.alt" class="flex-shrink w-1/4 object-cover">
+    </NuxtLink>
   </div>
 </template>
 <script>
-export default {}
+export default {
+  async asyncData ({
+    $content,
+    params
+  }) {
+    const blogs = await $content('blog', params.slug)
+      .only(['title', 'subtitle', 'slug', 'cover', 'createdAt'])
+      .sortBy('createdAt', 'asc')
+      .fetch()
+
+    return {
+      blogs
+    }
+  }
+}
 </script>
